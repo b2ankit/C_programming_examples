0e016ada4ebf3d9daec0d4f34ff144057baa3972
b2ankit <ankit15ec007@gmail.com>
2020-07-11 02:11:35 +0530

    pointer3 added

---
+++ basic/pointer3.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+
+    int x =10;
+    int y = 100;
+    const int *constant_pointer_num = NULL;
+    int *pointer_num = NULL;
+    pointer_num=&x;
+    constant_pointer_num = &y;
+    printf("Value of x : %d\n",*pointer_num);
+    printf("Value of x : %d\n",*constant_pointer_num);
+    x=20;
+    y=200;
+    printf("Modified value of x using value: %d\n",*pointer_num);
+    printf("Modified value of x using value: %d\n",*constant_pointer_num);
+    *pointer_num=30;
+    printf("Modified value of x using pointer : %d\n",*pointer_num);
+
+    /* when we using const before pointer we can't modifer the value of 
+    variable using pointer*/
+
+    // *constant_pointer_num=300;
+    //  printf("Modified value of x using pointer : %d\n",*constant_pointer_num);
+
+
+
+
+    return 0;
+}
