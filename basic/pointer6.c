682b7b37b1fa7214d1da883c6d23a9fbe777aa00
b2ankit <ankit15ec007@gmail.com>
2020-07-11 14:18:41 +0530

    pointer6 modified

--- basic/pointer6.c
+++ basic/pointer6.c
@@ -3,13 +3,14 @@
 #include<string.h>
 
 int main(){
-    char name[] = "print string using pointer";
+    char name[] = "pointer6";
     char *p = name;
 
-    printf("String is : ");
+    printf("String is  \t address using pointer \t address using array\n");
     for(int i=0;i<strlen(name);i++){
-        printf("%c",*(p+i));
-
+        printf("  %c\t\t",*(p+i));
+        printf(" %zu \t",p+i);
+        printf(" %zu\n",&name[i]);
     }
     printf("\n");
 }
