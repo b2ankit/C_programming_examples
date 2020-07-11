2184c78486ea62016353995fd7e263cc0dbe35c7
b2ankit <ankit15ec007@gmail.com>
2020-07-12 00:09:03 +0530

    dynamic allocation example added

---
+++ basic/dynamic_allocation1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(){
+
+    int n;
+    printf("Enter the value of n : ");
+    scanf("%d",&n);
+    int *dynamic_pointer = NULL;
+    dynamic_pointer = (int *)malloc(n*sizeof(int));
+    printf("Enter the integer value\n");
+    for(int i=0;i<n;i++){
+        int value;
+        scanf("%d",&value);
+        *(dynamic_pointer+i) = value;
+    }
+
+    printf("Enter value is : ");
+    for(int i=0;i<n;i++){
+        printf(" %d\t",*(dynamic_pointer+i));
+    }
+    printf("\n");
+    return 0;
+}
